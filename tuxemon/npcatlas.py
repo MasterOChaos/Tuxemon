320da85add7afb964a1bed14b55816d3b91a531f
MasterOChaos <No>
2021-07-01 23:14:19 -0400

    initial test

---
+++ tuxemon/npcatlas.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import sys
+
+def createatlas(inpcdat):
+  # Add all of the images to a list 
+  inpc = '' + inpcdat
+  images = [Image.open(x) for x in [inpcdat+'_back_walk.001.png', inpcdat+'_back_walk.000.png', inpcdat+'_back_walk.002.png', inpcdat+'_back_walk.001.png', inpcdat+'_back_walk.000.png', inpc+'_back_walk.002.png']]
+  total_width = 0
+  max_height = 0
+
+
+  # find the width and height of the final image
+  for img in images:
+    total_width += img.size[0]
+    max_height = max(max_height, img.size[1])
+
+  # create a new image with the appropriate height and width
+  new_img = Image.new('RGB', (total_width, max_height))
+
+  # Write the contents of the new image
+  current_width = 0
+  for img in images:
+    new_img.paste(img, (current_width,0))
+    current_width += img.size[0]
+
+  # Save the image
+  new_img.save(iname + '.atlas.png') 
