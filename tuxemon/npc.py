32484fa02c3ae464093df2981761e042d8532ec1
MasterOChaos <73611161+MasterOChaos@users.noreply.github.com>
2021-07-17 01:26:29 -0400

    8

--- tuxemon/npc.py
+++ tuxemon/npc.py
@@ -204,38 +204,41 @@ class NPC(Entity):
 
         :return:
         """
-        # TODO: refactor animations into renderer
-        # Get all of the player's standing animation images.
-        self.standing = {}
-        for standing_type in facing:
-            filename = f"{self.sprite_name}_{standing_type}.png"
-            path = os.path.join("sprites", filename)
-            self.standing[standing_type] = load_and_scale(path)
+        if(ATLAS==1):
+            #TODO put loader code here
+        else:
+            # TODO: refactor animations into renderer
+            # Get all of the player's standing animation images.
+            self.standing = {}
+            for standing_type in facing:
+                filename = f"{self.sprite_name}_{standing_type}.png"
+                path = os.path.join("sprites", filename)
+                self.standing[standing_type] = load_and_scale(path)
 
-        self.playerWidth, self.playerHeight = self.standing["front"].get_size()  # The player's sprite size in pixels
+            self.playerWidth, self.playerHeight = self.standing["front"].get_size()  # The player's sprite size in pixels
 
-        # avoid cutoff frames when steps don't line up with tile movement
-        frames = 3
-        frame_duration = (1000 / CONFIG.player_walkrate) / frames / 1000 * 2
+            # avoid cutoff frames when steps don't line up with tile movement
+            frames = 3
+            frame_duration = (1000 / CONFIG.player_walkrate) / frames / 1000 * 2
 
-        # Load all of the player's sprite animations
-        anim_types = ["front_walk", "back_walk", "left_walk", "right_walk"]
-        for anim_type in anim_types:
-            images = [
-                "sprites/{}_{}.{}.png".format(self.sprite_name, anim_type, str(num).rjust(3, "0")) for num in range(4)
-            ]
+            # Load all of the player's sprite animations
+            anim_types = ["front_walk", "back_walk", "left_walk", "right_walk"]
+            for anim_type in anim_types:
+                images = [
+                    "sprites/{}_{}.{}.png".format(self.sprite_name, anim_type, str(num).rjust(3, "0")) for num in range(4)
+                ]
 
-            frames = []
-            for image in images:
-                surface = load_and_scale(image)
-                frames.append((surface, frame_duration))
+                frames = []
+                for image in images:
+                    surface = load_and_scale(image)
+                    frames.append((surface, frame_duration))
 
-            self.sprite[anim_type] = pyganim.PygAnimation(frames, loop=True)
+                self.sprite[anim_type] = pyganim.PygAnimation(frames, loop=True)
 
-        # Have the animation objects managed by a conductor.
-        # With the conductor, we can call play() and stop() on all the animation objects
-        # at the same time, so that way they'll always be in sync with each other.
-        self.moveConductor.add(self.sprite)
+            # Have the animation objects managed by a conductor.
+            # With the conductor, we can call play() and stop() on all the animation objects
+            # at the same time, so that way they'll always be in sync with each other.
+            self.moveConductor.add(self.sprite)
 
     def get_sprites(self, layer):
         """Get the surfaces and layers for the sprite
